af0110593eac714060b68668bd6d6b953f8cd02c
Brad <brad@viziworx.com>
2019-09-20 12:19:28 -0400

    Completed objects section

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,30 +5,62 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
-
+class Dinosaur {
+  constructor(dinosaurAttrs){
+    this.name = dinosaurAttrs.name;
+    this.diet = dinosaurAttrs.diet;
+    this.weight = dinosaurAttrs.weight;
+    this.length = dinosaurAttrs.length;
+    this.period = dinosaurAttrs.period;
+    this.roar = dinosaurAttrs.roar;
+  }
+}
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyrannosaurus = new Dinosaur({
+  name: 'Tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  roar: function() {
+    return "RAWERSRARARWERSARARARRRR!"
+  }
+})
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosaurus = new Dinosaur({
+  name: 'Stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+})
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const velociraptor = new Dinosaur({
+  name: 'Velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+})
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(`The ${tyrannosaurus.name} weighs a hefty ${tyrannosaurus.weight}. Lay off that second serving of ${stegosaurus.name}, Mr. ${tyrannosaurus.name} `);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(`The ${velociraptor.name}'s diet is ${velociraptor.diet}.`);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
-
+console.log(tyrannosaurus.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
